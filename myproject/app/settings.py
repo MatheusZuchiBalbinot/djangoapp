965804389f2b57ba9432e62c7e89b868304a541d
Matheus Zuchi Balbinot <104937774+MatheusZuchiBalbinot@users.noreply.github.com>
2024-06-26 21:41:46 -0300

    merge dev into main (#2)
    * initial structure
    
    * feat: adding postgresql connection
    
    * fix: docker files

--- myproject/app/settings.py
+++ myproject/app/settings.py
@@ -73,17 +73,16 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'PASSWORD': 'mypassword',
-#         'HOST': 'my-postgres',
-#         'PORT': '5432',
-#     }
-# }
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'mypassword',
+        'HOST': 'my-postgres',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
